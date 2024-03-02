2002ed3e4260b1311c18006d4b43242494c480ce
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-03-02 18:26:34 -0500

    naturalSum

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+/* sum of all natural numbers from 1 to n*/
+
+function sumOfNaturalNumber(num) {
+
+    let sum = 0;
+        for(let i = 1; i<num; i++){
+            sum = sum + i;
+    }
+
+    return sum;
+}
+
